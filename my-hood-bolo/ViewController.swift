4765a51c40162dd6d6618b8c568ee630fb5a9ab5
Dide van Berkel <didevanberkel@macbook-air-van-dide.local>
2016-03-13 09:54:37 +0100

    Initial Commit

---
+++ my-hood-bolo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-hood-bolo
+//
+//  Created by Dide van Berkel on 13-03-16.
+//  Copyright © 2016 Gary Grape Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
